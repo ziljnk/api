33fbe2c3dc0ba769565192e89f11f5acf1276e01
mchis1908 <mchis19.08@gmail.com>
2023-12-28 23:58:22 +0700

    updated

--- src/routes/conversationRoutes.js
+++ src/routes/conversationRoutes.js
@@ -5,5 +5,6 @@ const conversationController = require('../controllers/conversationController');
 router.post('/', conversationController.createConversationRoom)
 router.get('/:userId', conversationController.getAllConversations)
 router.post('/group', conversationController.createGroupChatRoom)
+router.patch('/pinMessage', conversationController.pinMessage)
 
 module.exports = router;
--- src/routes/messageRoutes.js
+++ src/routes/messageRoutes.js
@@ -4,6 +4,5 @@ const messageController = require('../controllers/messageController');
 
 router.post('/', messageController.createMessage);
 router.post('/delete', messageController.deleteMessage);
-router.patch('/pinMessage', conversationController.pinMessage)
 
 module.exports = router;
